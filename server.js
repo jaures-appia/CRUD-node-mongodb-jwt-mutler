55f8c3f97b2e0c7646074448e5f8e98e96f3705e
jaures-appia <47213767+jaures-appia@users.noreply.github.com>
2020-07-28 10:07:33 +0000

    ajout de de multer
    ajout de multer pour l'enregistrement des fichiers

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const stuffRoutes = require('./routes/stuffs')
 const userRoutes = require('./routes/users');
@@ -21,6 +22,7 @@ useUnifiedTopology: true })
     .catch(() => console.log('Connexion à MongoDB échouée !'));
   
 app.use(bodyParser.json())
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/stuff', stuffRoutes)
 app.use('/api/auth', userRoutes);
